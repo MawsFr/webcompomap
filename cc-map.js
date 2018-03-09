71c67a1e89393a3cf46db73a9323448dbab20a15
codeconcept <contact@codeconcept.fr>
2018-03-09 12:41:47 +0100

    changing zoom level when zoom attribute changes

--- cc-map.js
+++ cc-map.js
@@ -4,10 +4,18 @@ class CcMap extends HTMLElement {
         // store shadow root reference in a _root variable
         this._root = this.attachShadow({ mode: 'open' }); 
         console.log('this._root',this._root);
+
+        // data
+        this.rennes = {
+            lat: 48.1173,
+            lng: -1.6778
+        };
+        this._componentReady = false;
     }
     
     connectedCallback() {
         console.log('cc-map added to the DOM');
+        this._componentReady = true;
         this._root.innerHTML = `
         <style>
         #map {
@@ -27,16 +35,13 @@ class CcMap extends HTMLElement {
             console.log('Google maps NOT ready');
             return;
         }
-        const rennes = {
-            lat: 48.1173,
-            lng: -1.6778
-        };
-        this._initMap(rennes);
+       
+        this._initMap(this.rennes, 12);
     }
 
-    _initMap(center) {
+    _initMap(center, zoom) {
         var map = new window.google.maps.Map(this._mapDiv, {
-            zoom: 12,
+            zoom: zoom,
             center: center
         });
     }
@@ -46,7 +51,12 @@ class CcMap extends HTMLElement {
     }
 
     attributeChangedCallback(name, oldValue, newValue) {
+        if(!this._componentReady) return;
+
         console.log(`name: ${name} - old value: ${oldValue} - new value: ${newValue}`);
+        if(name === 'zoom') {            
+            this._initMap(this.rennes, parseInt(newValue));
+        }
     }
 
 } // end class
